a5378fa485d4c83a16664ffedd4fc6b3cd0ddc2c
vijay <vijaykumarjavlia@gmail.com>
2021-08-31 11:48:49 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,47 @@
+
+
+
+         let oldplayer=document.querySelector('#my-video');
+        //  videojs(oldplayer).dispose();
+    let count=0;
+        document.querySelector('#prev').addEventListener('dblclick', ()=>{ backward()});
+
+        document.querySelector('#next').addEventListener('dblclick',()=>{forward()});
+
+        // document.querySelector('#next').addEventListener('dblclick',()=>
+        // {
+        //     alert("call Next ");
+        // })
+
+        var videoplayer=videojs('my-video',{
+            // controls:true,
+             loop:true,
+            playbackRates:[0.5,1,1.5,2,2.5,3,3.5]
+           
+            });
+            // id="my-video_html5_api"
+
+            videoplayer.seekButtons({
+                forward: 30,
+                back: 10
+            })
+
+
+            const  skip=(time)=>
+            {
+
+                videoplayer.currentTime(videoplayer.currentTime()+time);
+
+            }
+
+            const forward=()=>{
+            skip(30)
+            }
+
+            const backward=()=>{
+                skip(-10)
+            }
+
+
+
+
